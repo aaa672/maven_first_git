b02bced7c7da40bf7d83622fcb4da73fcae316d4
liu sen <qq602901800@gmail.com>
2019-11-25 14:40:35 +0800

    maven_first_git

---
+++ src/test/java/com/hn/first/action/user/UserLoginServletTest.java
@@ -0,0 +1,13 @@
+package com.hn.first.action.user;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserLoginServletTest {
+
+    @Test
+    public void doPost() {
+
+    }
+}
